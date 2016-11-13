46eed129edacd6bf1c5ff0e3f83d4ba6427f4896
Jo Jangmi <wkdal720@gmail.com>
2016-11-13 11:54:50 -0800

    rating page user i/o working

--- app/controllers/api/ratings_controller.rb
+++ app/controllers/api/ratings_controller.rb
@@ -1,11 +1,4 @@
 class Api::RatingsController < ApplicationController
-  # we can make this better with
-  # store rating_id in session store
-  # when user sends existing one,
-  # use PUT, when user sends non-existing one,
-  # user POST
-  # to reduce User, Card querying
-  # we can do that in the future : )
   def rate
     @rating = Rating.find_or_create_by(user_id: params[:user_id], card_id: params[:card_id])
     @rating.rating = params[:rating]
--- frontend/components/card/card_container.js
+++ frontend/components/card/card_container.js
@@ -5,6 +5,7 @@ import { receiveRating } from './../../actions/session_actions.js';
 const mapStateToProps = (state) => ({
   card: state.card,
   userId: state.session.id,
+  ratings: state.session.ratings
 });
 
 const mapDispatchToProps = (dispatch) => ({
