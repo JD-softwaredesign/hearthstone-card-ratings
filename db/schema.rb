28ee8cf37922eb7947267987b262905869882ec8
Jo Jangmi <wkdal720@gmail.com>
2016-11-20 09:46:07 -0800

    cards view styled

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161120001049) do
+ActiveRecord::Schema.define(version: 20161120040915) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -29,6 +29,7 @@ ActiveRecord::Schema.define(version: 20161120001049) do
     t.boolean  "released",     null: false
     t.datetime "created_at",   null: false
     t.datetime "updated_at",   null: false
+    t.string   "rarity"
   end
 
   add_index "cards", ["expansion_id"], name: "index_cards_on_expansion_id", using: :btree
