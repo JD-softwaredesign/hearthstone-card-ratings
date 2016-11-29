46403dca50db20aa159ed6eb88150129f94b5fcc
Jo Jangmi <wkdal720@gmail.com>
2016-11-28 18:37:52 -0800

    after released, card rating is locked frontend/backend

--- app/controllers/api/ratings_controller.rb
+++ app/controllers/api/ratings_controller.rb
@@ -1,8 +1,12 @@
 class Api::RatingsController < ApplicationController
   def rate
     @rating = Rating.find_or_create_by(user_id: params[:user_id], card_id: params[:card_id])
-    @rating.rating = params[:rating]
-    @rating.save
-    render json: { params[:card_id] => @rating[:rating] }
+    if @rating.card.released
+      render json: 'This card is already released', status: 403
+    else
+      @rating.rating = params[:rating]
+      @rating.save
+      render json: { params[:card_id] => @rating[:rating] }
+    end
   end
 end
